04e756dcc21ebae0ba90e80d407b3361480b1f2d
Denis Rogozin <133758633+Barmaley0@users.noreply.github.com>
2024-05-27 18:55:40 +0300

    level_1 done

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,4 +2,7 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    assert build_url("sberbank.ru", "page1") == "sberbank.ru/page1"
+    assert build_url("sberbank.ru", "page1", {"param1": "value1"}) == "sberbank.ru/page1?param1=value1"
+    assert build_url("sberbank.ru", "page1", {"param1": "value1", "param2": "value2"}) == "sberbank.ru/page1?param1=value1&param2=value2"
+    assert build_url("sberbank.ru", "page1", {"param1": "value+with+space", "param2": "value%20with%20urlencoding"}) == "sberbank.ru/page1?param1=value+with+space&param2=value%20with%20urlencoding"
--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,10 @@
+import datetime
 from functions.level_1.two_date_parser import compose_datetime_from
 
 
 def test_compose_datetime_from():
-    pass
+    today = datetime.date.today()
+    assert compose_datetime_from(str(today), "15:00") == datetime.datetime(today.year, today.month, today.day, 15, 00)
+
+    tomorrow = today + datetime.timedelta(days=1)
+    assert compose_datetime_from("tomorrow", "15:30") == datetime.datetime(tomorrow.year, tomorrow.month, tomorrow.day, 15, 30)
